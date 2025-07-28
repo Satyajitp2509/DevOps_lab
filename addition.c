47367a1936dfa9244be6b5c6d7ed686487fdbf38
satyajit <2203035@ritindia.edu>
2025-07-28 16:29:31 +0530

    updated addition with new linegit

---
+++ addition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{int a;
+int b;
+printf("Enter the value of a : ");
+scanf("%d",&a);
+
+printf("Enter the value of b : ");
+scanf("%d",&b);
+
+int sum;
+sum=a+b;
+printf("Sum : %d",sum);
+}
